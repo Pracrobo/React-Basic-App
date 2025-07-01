3ee25f11890e89db6c6774532a2f0fb177f4b8f2
Pracrobo <cpu180621@gmail.com>
2025-07-01 17:50:44 +0900

    [react] 1.componen화

--- react-basic/src/main.jsx
+++ react-basic/src/main.jsx
@@ -1,6 +1,9 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
+// const Hello = () => {
+//   return <div>hello</div>;
+// };
+// createRoot(document.getElementById("root")).render(<Hello />);
 createRoot(document.getElementById("root")).render(<App />);
