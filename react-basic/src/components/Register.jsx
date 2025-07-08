af462b04b971899e34a60f420b64de5959aade75
Pracrobo <cpu180621@gmail.com>
2025-07-08 09:44:37 +0900

    [react] useRef 함수 이론 및 실습

--- react-basic/src/components/Register.jsx
+++ react-basic/src/components/Register.jsx
@@ -7,20 +7,36 @@ const Register = () => {
         birth: "",
     })    
 
-    const refObj = useRef(0);
-    console.log(refObj);
+    const countRef = useRef(0); // ref Object
+    const inputRef = useRef()
+    // console.log("Reference 초기화"); // {current: 0}
 
     const onChange = (e) => {
+        countRef.current++;
+        console.log(countRef.current); 
         setInput({
             ...input,
             [e.target.name] : e.target.value,
         })
     };
-    
+    const onSubmit = () => {
+        if(input.name === "") {
+            // 이름을 입력하는 DOM  요소 포커스
+            inputRef.current.focus();
+        }
+    };
+
     return (
     <div>
-        <div><input name="name" value={input.name} onChange={onChange} placeholder={"이름"}></input></div>
+        {/* <button onClick={() => {
+            refObj.current++;
+            console.log(refObj.current);
+        }}>
+            ref + 1
+        </button> */}
+        <div><input ref={inputRef} name="name" value={input.name} onChange={onChange} placeholder={"이름"}></input></div>
         <div><input name="birth" value={input.birth} onChange={onChange} type="date"></input>{input.birth}</div>
+        <button onClick={onSubmit}>제출</button>
     </div>
     )
 }
