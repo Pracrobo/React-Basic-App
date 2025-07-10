92d1135d1f7866309164ecccfe1d786f06d8d8a7
Pracrobo <cpu180621@gmail.com>
2025-07-10 18:00:45 +0900

    [react] 추가

--- todolist-app/src/App.jsx
+++ todolist-app/src/App.jsx
@@ -2,8 +2,32 @@ import "./App.css";
 import Header from "./components/Header";
 import Editor from "./components/Editor";
 import List from "./components/List";
+import { useState } from "react";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "리액트 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "typescript 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "카프카 공부하기",
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
+  const [todos, setTodo] = useState([mockData]);
+
   return (
     <div className="App">
       <Header></Header>
