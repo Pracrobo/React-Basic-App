ff0ea21671a154194d54f5758978f12ec5bc115c
Pracrobo <cpu180621@gmail.com>
2025-07-22 13:56:02 +0900

    [react] 감정일기 - Date 관련 정리 필요 - mock 데이터 props로 데이터 전달 - context를 이용한 import 후 값 변경 useState 저장 (context-drilling 대신)

---
+++ emotion-notes-app/src/components/DiaryList.jsx
@@ -0,0 +1,49 @@
+import Button from "./Button";
+import "./DiaryList.css";
+import DiaryItem from "./DiaryItem";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+const DiaryList = ({ data }) => {
+  const nav = useNavigate();
+  const [sortType, setSortType] = useState("latest");
+
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  };
+  const getSortedDate = () => {
+    return data.toSorted((a, b) => {
+      //객체값 비교안되서 비교함수 콜백함수로 넣어야함
+      if (sortType === "oldest") {
+        return Number(a.createdDate) - Number(b.createdDate);
+      } else {
+        return Number(b.createdDate) - Number(a.createdDate);
+      }
+    });
+  };
+
+  const sortedData = getSortedDate();
+
+  return (
+    <div className="DiaryList">
+      <div className="menu_bar">
+        <select onChange={onChangeSortType}>
+          <option value={"latest"}>최신순</option>
+          <option value={"oldest"}>오래된 순</option>
+        </select>
+        <Button
+          onClick={() => nav(`/new`)}
+          text={"새 일기 쓰기"}
+          type={"POSITIVE"}
+        ></Button>
+      </div>
+      <div className="list_wrapper">
+        {sortedData.map((item) => (
+          <DiaryItem key={item.id} {...item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DiaryList;
--- emotion-notes-app/src/pages/Home.jsx
+++ emotion-notes-app/src/pages/Home.jsx
@@ -1,10 +1,54 @@
-import { useSearchParams } from "react-router-dom";
+import { useState, useContext } from "react";
+import { DiaryStateContext } from "../App";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import DiaryList from "../components/DiaryList";
 
+const getMonthlyData = (pivotDate, data) => {
+  const beginTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth(),
+    1,
+    0,
+    0,
+    0
+  ).getTime(); //1일 0시0분0초
+  const endTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth() + 1,
+    0,
+    23,
+    59,
+    59
+  ).getTime();
+  // 다음달의 0일 (이전달의 마지막 달 : -1)
+  return data.filter(
+    (item) => beginTime <= item.createdDate && item.createdDate <= endTime
+  ); // 이번달 데이터
+};
 const Home = () => {
-  const [params, setParams] = useSearchParams();
-  console.log(params.get("value"));
+  const data = useContext(DiaryStateContext);
+  const [pivotDate, setPivotDate] = useState(new Date());
 
-  return <div>Home</div>;
+  const monthlyData = getMonthlyData(pivotDate, data);
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
+  return (
+    <div>
+      <Header
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`} // get Date는 0부터 계산
+        leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
+        rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
+      />
+      <DiaryList data={monthlyData}></DiaryList>
+    </div>
+  );
 };
 
 export default Home;
