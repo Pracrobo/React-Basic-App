635757ea528865532dfc0e206a774e82b158ecff
Pracrobo <cpu180621@gmail.com>
2025-07-22 15:36:35 +0900

    [react] 감정일기 edit 화면 - 공통 컴포넌트 확인하기 - justify-content : space-around, space-between

--- emotion-notes-app/src/pages/New.jsx
+++ emotion-notes-app/src/pages/New.jsx
@@ -1,5 +1,17 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+
 const New = () => {
-  return <div>New</div>;
+  return (
+    <div>
+      <Header
+        title={"새일기쓰기"}
+        leftChild={<Button text={"< 뒤로가기"}></Button>}
+      ></Header>
+      <Editor></Editor>
+    </div>
+  );
 };
 
 export default New;
