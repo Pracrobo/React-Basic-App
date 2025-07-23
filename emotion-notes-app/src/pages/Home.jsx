3ac4afbcc2260eedbabe046b04b0f8003aedca08
Pracrobo <cpu180621@gmail.com>
2025-07-23 15:52:16 +0900

    [react] 감정일기 - Home 페이지 - title 변경

--- emotion-notes-app/src/pages/Home.jsx
+++ emotion-notes-app/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { DiaryStateContext } from "../contexts/DiaryContext";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
-
+import useTitle from "../hooks/useTitle";
 const getMonthlyData = (pivotDate, data) => {
   const beginTime = new Date(
     pivotDate.getFullYear(),
@@ -28,6 +28,7 @@ const getMonthlyData = (pivotDate, data) => {
   ); // 이번달 데이터
 };
 const Home = () => {
+  useTitle("감정 일기");
   const data = useContext(DiaryStateContext);
   const [pivotDate, setPivotDate] = useState(new Date());
 
