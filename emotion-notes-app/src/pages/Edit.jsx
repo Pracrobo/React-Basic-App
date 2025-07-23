a6d467d0936e73c0db516fe221b977c994d2afd6
Pracrobo <cpu180621@gmail.com>
2025-07-23 15:47:21 +0900

    [react] 감정일기 - 수정페이지 - 컴포넌트 props 전달 및 함수로 전달 보기

--- emotion-notes-app/src/pages/Edit.jsx
+++ emotion-notes-app/src/pages/Edit.jsx
@@ -1,6 +1,59 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+
+import {
+  DiaryDispatchContext,
+  DiaryStateContext,
+} from "../contexts/DiaryContext";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import useDiary from "../hooks/useDiary";
+import useTitle from "../hooks/useTitle";
+
 const Edit = () => {
   const params = useParams();
-  return <div> {params.id} 번 Edit</div>;
+  useTitle(`${params.id}번 일기 수정`);
+  const nav = useNavigate();
+  const { onUpdate, onDelete } = useContext(DiaryDispatchContext);
+
+  const currentDiaryItem = useDiary(params.id);
+  const onClickDelete = () => {
+    if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않습니다.")) {
+      onDelete(params.id);
+      nav("/", { replace: true });
+    }
+  };
+
+  const onSubmit = (input) => {
+    if (window.confirm("일기를 정말 수정할까요?")) {
+      onUpdate(
+        params.id,
+        input.createdDate.getTime(),
+        input.emotionId,
+        input.content
+      );
+      nav("/", { replace: true });
+    }
+  };
+
+  return (
+    <div>
+      <Header
+        title={"일기 수정하기"}
+        leftChild={
+          <Button onClick={() => nav(-1)} text={"< 뒤로가기"}></Button>
+        }
+        rightChild={
+          <Button
+            onClick={onClickDelete}
+            text={"삭제하기"}
+            type={"NEGATIVE"}
+          ></Button>
+        }
+      ></Header>
+      <Editor initData={currentDiaryItem} onSubmit={onSubmit} />
+    </div>
+  );
 };
 export default Edit;
