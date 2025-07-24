eda452b4bae15fa54eea785702ec22ebcc6552f0
Pracrobo <cpu180621@gmail.com>
2025-07-24 14:31:37 +0900

    [nodejs] - 라이브러리

---
+++ nodejs/lib.md
@@ -0,0 +1,5 @@
+## [npm js]('https://www.npmjs.com/')
+
+package-json vs package-lock.json 차이
+
+- lock : 실제 설치 버전 정보
--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -7,6 +7,10 @@
 // import mul from "./math2.js"; // 이름 변경도 가능 기본값만
 
 import mul, { add, sub } from "./math2.js"; // 다 넣어도 됨
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
 
 console.log(add(1, 2));
 console.log(sub(1, 2));
