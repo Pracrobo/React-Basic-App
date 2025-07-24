39b6b8f3730146c27759145cc5e55400478cb6e9
Pracrobo <cpu180621@gmail.com>
2025-07-24 14:18:08 +0900

    [node] 모듈 시스템 이해

---
+++ node/src/index.js
@@ -0,0 +1,13 @@
+// const { add, sub } = require("./math");
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// // console.log(moduleData);
+
+// import { add, sub } from "./math2.js"; // 확장자 넣기 esm
+// import mul from "./math2.js"; // 이름 변경도 가능 기본값만
+
+import mul, { add, sub } from "./math2.js"; // 다 넣어도 됨
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(1, 2));
